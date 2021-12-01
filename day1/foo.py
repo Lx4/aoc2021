5de0e0564d7104704b67915c8109b93c15399875
Franck Farcy <franck.farcy@gmail.com>
2021-12-01 16:03:30 +0100

    small improvements

--- day1/foo.py
+++ day1/foo.py
@@ -3,11 +3,9 @@ from input import data
 
 def sonar(array):
     counter = 0
-    for i in range(len(array)):
-        if i == 0:
-            continue
+    for i in range(len(array) -1):
         if (array[i] > array[i-1]):
-            counter = counter + 1
+            counter+=1
     return counter
 
 # part 1  
